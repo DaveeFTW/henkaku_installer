748e5cea018f5cc6dee149b88f2e82f9671963c0
Davee <davee@x-fusion.co.uk>
2016-11-27 23:10:17 +0000

    add stub installer executable

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <framework/viewmanager.h>
+
+#include <psp2/kernel/threadmgr.h>
+
+//#### TODO: move to SDK
+extern "C"
+{
+	unsigned int sleep(unsigned int seconds)
+	{
+		sceKernelDelayThread(seconds*1000*1000);
+		return 0;
+	}
+	
+	int usleep(unsigned int usec)
+	{
+		sceKernelDelayThread(usec);
+		return 0;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	ViewManager manager;
+	manager.exec();
+}
