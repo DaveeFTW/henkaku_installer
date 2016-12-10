f90f1e6fef5a89a367528545368bc8995825b3d1
Davee <davee@x-fusion.co.uk>
2016-12-10 09:36:29 +0000

    add glm and gxm texture

---
+++ framework/include/framework/gxmtexture.h
@@ -0,0 +1,93 @@
+/*
+ * gxmtexture.h - encapsulate a single gxm texture
+ *
+ * Copyright (C) 2016 David "Davee" Morgan
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+#ifndef GXMTEXTURE_H
+#define GXMTEXTURE_H
+
+#include <memory>
+#include <cstdint>
+
+struct SceGxmTexture;
+struct SceGxmContext;
+
+template <typename T>
+class GpuMemoryBlock;
+
+class GxmTexture
+{
+public:
+	enum CoordinateDirection
+	{
+		DirectionU,
+		DirectionV
+	};
+
+	enum WrapMode
+	{
+		Repeat,
+		MirroredRepeat,
+		ClampToEdge,
+		MirrorClamp,
+		RepeatIgnoreBorder,
+		ClampFullBorder,
+		ClampIgnoreBorder,
+		ClampHalfBorder
+	};
+
+	enum Filter
+	{
+		Point,
+		Linear
+	};
+
+	enum class MipFilter
+	{
+		Disabled,
+		Enabled
+	};
+
+	enum TextureFormat
+	{
+		ARGB8
+	};
+
+public:
+	GxmTexture(void);
+	~GxmTexture(void) = default;
+
+	void allocateStorage(void);
+	bool isStorageAllocated(void) const;
+
+	void bind(SceGxmContext *ctx, std::uint32_t unit);
+	void setSize(std::size_t width, std::size_t height = 1, std::size_t depth = 1);
+
+	std::size_t width(void) const { return m_width; }
+	std::size_t height(void) const { return m_height; }
+	std::size_t depth(void) const { return m_depth; }
+
+	void setMinificationFilter(Filter filter);
+	void setMagnificationFilter(Filter filter);
+	void setMinMagFilter(Filter min, Filter mag);
+	void setWrapMode(CoordinateDirection direction, WrapMode mode);
+	void setWrapMode(WrapMode mode);
+
+	void setFormat(TextureFormat format);
+
+	void setData(const void *data);
+
+private:
+	std::unique_ptr<SceGxmTexture> m_texture;
+	std::unique_ptr<GpuMemoryBlock<char>> m_storage;
+	std::size_t m_width{0};
+	std::size_t m_height{1};
+	std::size_t m_depth{1};
+	TextureFormat m_format;
+};
+
+#endif //GXMTEXTURE_H
---
+++ framework/src/gxmtexture.cpp
@@ -0,0 +1,178 @@
+/*
+ * gxmtexture.cpp - encapsulate a single gxm texture
+ *
+ * Copyright (C) 2016 David "Davee" Morgan
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+#include <framework/gxmtexture.h>
+#include <framework/gpumemoryblock.h>
+
+#include <easyloggingpp/easylogging++.h>
+
+#include <psp2/gxm.h>
+
+namespace
+{
+	constexpr SceGxmTextureAddrMode convertWrapMode(GxmTexture::WrapMode mode)
+	{
+		switch (mode)
+		{
+		default:
+		case GxmTexture::Repeat:
+			return SCE_GXM_TEXTURE_ADDR_REPEAT;
+		case GxmTexture::MirroredRepeat:
+			return SCE_GXM_TEXTURE_ADDR_MIRROR;
+		case GxmTexture::ClampToEdge:
+			return SCE_GXM_TEXTURE_ADDR_CLAMP;
+		case GxmTexture::MirrorClamp:
+			return SCE_GXM_TEXTURE_ADDR_MIRROR_CLAMP;
+		case GxmTexture::RepeatIgnoreBorder:
+			return SCE_GXM_TEXTURE_ADDR_REPEAT_IGNORE_BORDER;
+		case GxmTexture::ClampFullBorder:
+			return SCE_GXM_TEXTURE_ADDR_CLAMP_FULL_BORDER;
+		case GxmTexture::ClampIgnoreBorder:
+			return SCE_GXM_TEXTURE_ADDR_CLAMP_IGNORE_BORDER;
+		case GxmTexture::ClampHalfBorder:
+			return SCE_GXM_TEXTURE_ADDR_CLAMP_HALF_BORDER;
+		}
+	}
+
+	constexpr SceGxmTextureMipFilter convertFilter(GxmTexture::MipFilter filter)
+	{
+		switch (filter)
+		{
+		default:
+		case GxmTexture::MipFilter::Disabled:
+			return SCE_GXM_TEXTURE_MIP_FILTER_DISABLED;
+		case GxmTexture::MipFilter::Enabled:
+			return SCE_GXM_TEXTURE_MIP_FILTER_ENABLED;
+		}
+	}
+
+	constexpr SceGxmTextureFilter convertFilter(GxmTexture::Filter filter)
+	{
+		switch (filter)
+		{
+		default:
+		case GxmTexture::Point:
+			return SCE_GXM_TEXTURE_FILTER_POINT;
+		case GxmTexture::Linear:
+			return SCE_GXM_TEXTURE_FILTER_LINEAR;
+		}
+	}
+
+	constexpr SceGxmTextureFormat convertTextureFormat(GxmTexture::TextureFormat format)
+	{
+		switch (format)
+		{
+		default:
+		case GxmTexture::ARGB8:
+			return SCE_GXM_TEXTURE_FORMAT_A8R8G8B8;
+		}
+	}
+
+	constexpr std::size_t texturePixelSize(GxmTexture::TextureFormat format)
+	{
+		switch (format)
+		{
+		default:
+		case GxmTexture::ARGB8:
+			return 4;
+		}
+	}
+} // anonymous namespace
+
+GxmTexture::GxmTexture(void)
+	: m_texture(new SceGxmTexture)
+{
+	std::memset(m_texture.get(), 0, sizeof(SceGxmTexture));
+}
+
+void GxmTexture::allocateStorage(void)
+{
+	if (m_storage)
+		return;
+
+	m_storage = std::make_unique<GpuMemoryBlock<char>>
+	(
+		  m_width*m_height*m_depth*texturePixelSize(m_format)
+		, SCE_GXM_MEMORY_ATTRIB_READ
+		, SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW
+	);
+}
+
+bool GxmTexture::isStorageAllocated(void) const
+{
+	return m_storage != nullptr;
+}
+
+void GxmTexture::bind(SceGxmContext *ctx, std::uint32_t unit)
+{
+	sceGxmSetFragmentTexture(ctx, unit, m_texture.get());
+}
+
+void GxmTexture::setSize(std::size_t width, std::size_t height, std::size_t depth)
+{
+	m_width = width;
+	m_height = height;
+	m_depth = depth;
+}
+
+void GxmTexture::setData(const void *data)
+{
+	std::memcpy(m_storage->address(), data, m_width*m_height*m_depth*texturePixelSize(m_format));
+	
+	auto res = sceGxmTextureInitLinear
+	(
+		m_texture.get(), 
+		m_storage->address(), 
+		convertTextureFormat(m_format),
+		m_width,
+		m_height,
+		0
+	);
+
+	LOG(INFO) << "sceGxmTextureInitLinear: " << res;
+}
+
+void GxmTexture::setMinificationFilter(Filter filter)
+{
+	sceGxmTextureSetMinFilter(m_texture.get(), convertFilter(filter));
+}
+
+void GxmTexture::setMagnificationFilter(Filter filter)
+{
+	sceGxmTextureSetMagFilter(m_texture.get(), convertFilter(filter));
+}
+
+void GxmTexture::setMinMagFilter(Filter min, Filter mag)
+{
+	setMinificationFilter(min);
+	setMagnificationFilter(mag);
+}
+
+void GxmTexture::setWrapMode(CoordinateDirection direction, WrapMode mode)
+{
+	if (direction == DirectionU)
+	{
+		sceGxmTextureSetUAddrMode(m_texture.get(), convertWrapMode(mode));
+	}
+	else if (direction == DirectionV)
+	{
+		sceGxmTextureSetVAddrMode(m_texture.get(), convertWrapMode(mode));
+	}
+}
+
+void GxmTexture::setWrapMode(WrapMode mode)
+{
+	setWrapMode(DirectionU, mode);
+	setWrapMode(DirectionV, mode);
+}
+
+void GxmTexture::setFormat(TextureFormat format)
+{
+	m_format = format;
+}
